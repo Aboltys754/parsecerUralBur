0707fba98f89893258333e3304352745ad62754a
Aboltys754 <warwar754@mail.ru>
2024-11-04 11:24:00 +0500

    ...

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,16 @@ COPY ./requirements.txt /src/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /src/requirements.txt
 
+RUN pip install --no-cache-dir --upgrade -r /src/requirements.txt
+
 COPY . /src
 
 RUN wget https://storage.googleapis.com/chrome-for-testing-public/130.0.6723.91/linux64/chrome-linux64.zip
 
+RUN unzip chrome-linux64.zip
+
 RUN wget https://storage.googleapis.com/chrome-for-testing-public/130.0.6723.91/linux64/chromedriver-linux64.zip
+
+RUN unzip chromedriver-linux64.zip
 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- parser.py
+++ parser.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
 
-service = Service(executable_path="chromedriver\\chromedriver.exe")
+service = Service(executable_path="chromedriver-linux64\\chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.binary_location = "chrome-linux64\\chrome.exe"
 
 def parser_url(url: str):
     try:
-        browser = webdriver.Chrome(service=service)
+        browser = webdriver.Chrome(service=service, options=options)
         browser.implicitly_wait(5)
         browser.get(url)
         html = browser.page_source
